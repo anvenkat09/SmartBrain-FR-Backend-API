1559b93e177377050756c8496e01cfa9a586829b
Anirudh <anirudh.venkatesh@gmail.com>
2019-11-17 22:31:27 -0800

    smart brain api initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs')
+const cors = require('cors')
+const knex = require('knex')
+const register = require('./controllers/register')
+const signin = require('./controllers/signin')
+const profile = require('./controllers/profile')
+const image = require('./controllers/image')
+const PORT = process.env.PORT;
+
+const db = knex({ 
+	client: 'pg',
+	connection: {
+	  connectionString : process.env.DATABASE_URL,
+	  ssl: true
+	}
+});
+
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+
+const database = {
+	users: [
+		{
+			id: '123',
+			name: 'john',
+			email: 'john@gmail.com',
+			password: 'cookies',
+			entries: 0,
+			joined: new Date()
+		},
+		{
+			id: '124',
+			name: 'sally',
+			email: 'sally@gmail.com',
+			password: 'bananas',
+			entries: 0,
+			joined: new Date()
+		}
+	]
+}
+
+app.get('/', (req, res) => {
+	res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
+
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
+
+app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res, db)})
+
+app.put('/image', (req, res) => {image.handleImage(req, res, db)})
+
+app.post('/imageUrl', (req, res) => {image.handleApiCall(req, res)})
+
+app.listen(PORT, ()=>{
+	console.log(`app is running on port ${PORT}`);
+})
